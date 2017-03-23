c68740a6d96b57006025a446118d057f33366cc2
Mick van Gelderen <mickvangelderen@gmail.com>
2017-03-23 16:58:15 +0100

    :construction: Abstracting away all the manual stuff
    Have to generalize over more than 2 types
    Have to make sure map(...) is efficient and gives better compiling
    errors

--- exp/index.cpp
+++ exp/index.cpp
@@ -2,87 +2,84 @@
 #include <random>
 #include <cstdint>
 
-struct Result {
-    // Types
-public:
-    struct Amoeba {};
-
-    struct Robot {
-        uint32_t wheel_count;
-    };
-
-    struct Cat {
-        uint16_t leg_count;
-        uint64_t eye_count;
-    };
-
+template<typename A, typename B>
+struct Variant {
 private:
     enum class Type {
-        AMOEBA,
-        ROBOT,
-        CAT
+        AT,
+        BT
     };
 
     union Data {
-        Amoeba amoeba;
-        Robot robot;
-        Cat cat;
+        A a;
+        B b;
     };
 
-    // Fields
     const Type type;
     const Data data;
 
-    // Functions
 public:
-    Result(Amoeba amoeba) : type(Type::AMOEBA), data { .amoeba = amoeba } {};
+    Variant(A a) : type(Type::AT), data { .a = a } {};
+    Variant(B b) : type(Type::BT), data { .b = b } {};
 
-    Result(Robot robot) : type(Type::ROBOT), data { .robot = robot } {};
-
-    Result(Cat cat) : type(Type::CAT), data { .cat = cat } {};
-
-    const Amoeba * amoeba() {
-        return type == Type::AMOEBA ? &data.amoeba : nullptr;
-    }
-
-    const Robot * robot() {
-        return type == Type::ROBOT ? &data.robot : nullptr;
-    }
-
-    const Cat * cat() {
-        return type == Type::CAT ? &data.cat : nullptr;
+    // How do we write this to have map be able to determine the result?
+    // https://gist.github.com/tibordp/6909880
+    // http://stackoverflow.com/questions/5481539/what-does-t-double-ampersand-mean-in-c11
+    template<typename Result, typename MapA, typename MapB>
+    Result map(MapA map_a, MapB map_b) {
+        switch (type) {
+        case Type::AT: return map_a(data.a);
+        case Type::BT: return map_b(data.b);
+        }
     }
 };
+
+struct Robot {
+    uint32_t wheel_count;
+};
+
+struct Cat {
+    uint16_t leg_count;
+    uint64_t eye_count;
+};
+
+typedef Variant<Robot, Cat> Result;
 
 Result something() {
     std::random_device random_device;
     std::default_random_engine random_engine(random_device());
-    std::uniform_int_distribution<int> zero_one_or_two(0, 2);
-    int type = zero_one_or_two(random_engine);
+    std::uniform_int_distribution<int> zero_or_one(0, 1);
+    int type = zero_or_one(random_engine);
     switch (type) {
-    case 0: return Result { Result::Amoeba {} };
-    case 1: return Result { Result::Robot { .wheel_count = 3 } };
-    case 2: return Result { Result::Cat { .leg_count = 4, .eye_count = 2 } };
+    case 1: return Result { Robot { .wheel_count = 3 } };
+    case 2: return Result { Cat { .leg_count = 4, .eye_count = 2 } };
     }
-    exit(-1);
+    std::cerr << "Never should have come here!" << std::endl;
+    exit(EXIT_FAILURE);
 }
 
 int main() {
-    auto result = something();
-
-    std::cout << "Amoeba size: "<< sizeof(Result::Amoeba) << std::endl;
-    std::cout << "Robot size: "<< sizeof(Result::Robot) << std::endl;
-    std::cout << "Cat size: "<< sizeof(Result::Cat) << std::endl;
+    std::cout << "Robot size: "<< sizeof(Robot) << std::endl;
+    std::cout << "Cat size: "<< sizeof(Cat) << std::endl;
     std::cout << "Result size: "<< sizeof(Result) << std::endl;
 
-    if (result.amoeba()) {
-        std::cout << "Found an amoeba! It has no properties of interest to us." << std::endl;
-    } else if (auto robot = result.robot()) {
-        std::cout << "Wow its a robot! Beep boop I have " << robot->wheel_count << " wheels." << std::endl;
-    } else if (auto cat = result.cat()) {
-        std::cout << "Aww how adorable, it's a cat with " << cat->leg_count << " legs and " << cat->eye_count << " eyes." << std::endl;
-    } else {
-        std::cerr << "I have no idea what this is!" << std::endl;
-        exit(-1);
-    }
+    auto result = something();
+
+    auto& out = std::cout;
+
+    result.map<void>(
+        [&out] (const Robot& robot) {
+            out << "Wow its a robot! Beep boop I have " << robot.wheel_count << " wheels." << std::endl;
+        },
+        [&out] (const Cat& cat) {
+            out << "Aww how adorable, it's a cat with " << cat.leg_count << " legs and " << cat.eye_count << " eyes." << std::endl;
+        }
+    );
+
+    uint64_t total_count = result.map<uint64_t>(
+        [] (const Robot& robot) { return robot.wheel_count; },
+        [] (const Cat& cat) { return cat.leg_count + cat.eye_count; }
+    );
+
+    out << "At least the total number of things is " << total_count << std::endl;
 }
